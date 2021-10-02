2630603bc20c7878a5566bac9e257c350d02b483
soyeonvv <thdusdlvv@naver.com>
2021-10-02 17:08:24 +0900

    수정, 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React, { memo } from 'react';
 import './App.css';
 import plus from './plus.png';
 import Modal from './components/Modal';
+import ReModal from './components/ReModal';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       isModalOpen: false,
+      reModalOpen: false,
       memos: [
         {
           title : "첫번째 메모",
           author : "sy",
           content : "멋사 4-6시",
-        }
-      ]
+        },
+      ],
+      clickmemo:{
+        index:'',
+        title:'',
+        author:'',
+        content:'',
+      }
     }
   }
 
@@ -32,6 +40,46 @@ class App extends React.Component {
     this.setState({ memos: [...memos, new_memo] });
   }
 
+  handleRemove = (id) => {
+    let memos = this.state.memos;
+    this.setState({
+      memos: memos.filter((memo,index) => index !== id),
+    });
+  };
+
+  reopenModal = (index) => {
+    //reModal창을 열어주자
+    this.setState({
+    reModalOpen: true,
+    clickmemo:{
+    index:index,
+    title:this.state.memos[index].title,
+    author:this.state.memos[index].author,
+    content:this.state.memos[index].content,
+    }
+    });
+  }
+
+  recloseModal = () => {
+    this.setState({reModalOpen: false});
+  }
+
+  handleUpdate = (id, change_memo) => {
+    console.log(id);
+    console.log(change_memo);
+    let memos = this.state.memos;
+    this.setState({
+      memos: memos.map((memos,index) => {
+        if(index === id){
+          console.log(index+ '/'+ id);
+          return {id, ...change_memo};
+        }
+        return memos;
+      }),
+    });
+  };
+
+
   render() {
     return (
       <div className='container'>
@@ -41,7 +89,7 @@ class App extends React.Component {
             <tbody>
               <tr className='trList'>
                 {this.state.memos.map ( (memo,index) =>
-                  <td className='cell' key={index}>
+                  <td className='cell' key={index} onClick={()=>this.reopenModal(index)}>
                     <div className='inner'>
                       <h2> {memo.title} </h2>
                       <h5> {memo.author} </h5><br/><br/>
@@ -59,6 +107,9 @@ class App extends React.Component {
           </table>
           <main className='App'>
             <Modal isOpen={this.state.isModalOpen} close={this.closeModal} onCreate={this.handleCreate}/>
+            <ReModal reOpen={this.state.reModalOpen} reclose={this.recloseModal}
+                  data={this.state.clickmemo} onUpdate={this.handleUpdate}
+                  onRemove = {this.handleRemove}/>
           </main>
         </div>
       </div>
